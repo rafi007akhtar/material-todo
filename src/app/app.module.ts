9f229dfade0c3e54c27443661a08a8a45012d200
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-07 23:40:46 +0530

    created all components and laid them out for desktop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { LogoComponent } from './components/logo/logo.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CategoryComponent } from './components/category/category.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LogoComponent,
+    CategoriesComponent,
+    CategoryComponent,
+    TopNavComponent,
+    NewTaskComponent,
+    TaskListComponent,
+    TaskItemComponent
   ],
   imports: [
     BrowserModule,
