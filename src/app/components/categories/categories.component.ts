1e4d85544aaafc875f1ebdc5f88484507d729926
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-09 00:41:27 +0530

    Importing and computing the categories into the Categories component. Will rewrite this logic later.

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, computed, OnInit, signal } from '@angular/core';
+import { allCategories, CategoriesType } from '../../state/categories.signals';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit {
+  allCats = computed(allCategories);
+  // allCategories = signal<CategoriesType>([]);
 
+  ngOnInit(): void {
+    // this.allCategories.set([{ name: 'test 1' }, { name: 'test 2' }]);
+    // console.log(this.allCategories());
+  }
 }
