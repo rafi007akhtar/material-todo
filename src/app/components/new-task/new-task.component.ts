afd6e4459b767d2f5dbd5d0036ed04e3805e2d6b
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-09 19:55:25 +0530

    implemented basic look and feel of the new task component

--- src/app/components/new-task/new-task.component.ts
+++ src/app/components/new-task/new-task.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.scss'
+  styleUrl: './new-task.component.scss',
 })
-export class NewTaskComponent {
+export class NewTaskComponent implements OnInit {
+  constructor() {}
 
+  newTaskForm: FormGroup<any> = new FormGroup({});
+
+  ngOnInit(): void {
+    this.newTaskForm = new FormGroup({
+      taskName: new FormControl('', [Validators.required]),
+    });
+  }
+
+  formSubmit() {
+    console.log('form submission clicked');
+  }
 }
