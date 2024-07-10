9878fe60064a763e55af17d118ea93347f15f7d8
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-10 20:31:27 +0530

    implemented provision to add new category

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,17 +1,73 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
-import { allCategories, CategoriesType } from '../../state/categories.signals';
+import {
+  Component,
+  computed,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { allCategories } from '../../state/categories.signals';
+import { CategoriesType } from '../../models/category.model';
+import { TaskManagementService } from '../../services/task-management.service';
+import { Subscription } from 'rxjs';
+import { initCategories } from '../../data/dummy-data';
+import { MatSelectionListChange } from '@angular/material/list';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent implements OnInit {
-  allCats = computed(allCategories);
-  // allCategories = signal<CategoriesType>([]);
+export class CategoriesComponent implements OnInit, OnDestroy {
+  allCats: CategoriesType = [];
+  allCatsSup: Subscription | undefined;
+  addingNewCategory = signal(false);
+  newCategoryName: string = '';
+
+  @ViewChild('newCatInp') newCatInp!: ElementRef;
+
+  constructor(private tms: TaskManagementService) {}
 
   ngOnInit(): void {
-    // this.allCategories.set([{ name: 'test 1' }, { name: 'test 2' }]);
-    // console.log(this.allCategories());
+    this.allCatsSup = this.tms.allCategories$.subscribe((cats) => {
+      this.allCats = cats;
+    });
+
+    this.tms.setCategories(initCategories);
+    this.tms.setSelectedCategory('Default');
+  }
+
+  setSelectedCategory(change: MatSelectionListChange) {
+    const changedItem = change.options[0].value;
+    this.tms.setSelectedCategory(changedItem);
+  }
+
+  toggleAddCategory() {
+    this.addingNewCategory.update((val) => !val);
+    setTimeout(() => {
+      this.newCatInp?.nativeElement.focus();
+    }, 0);
+  }
+
+  onNewCatInputChange(evt: KeyboardEvent) {
+    if (evt.key === 'Enter') {
+      this.addNewCategory();
+    }
+  }
+
+  addCategoryHandler() {
+    this.toggleAddCategory();
+  }
+
+  addNewCategory() {
+    if (this.newCategoryName?.trim()) {
+      this.tms.addNewCategory(this.newCategoryName?.trim());
+      this.newCategoryName = '';
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.allCatsSup?.unsubscribe();
   }
 }
