096f7936497a682ec24ee254e1d16e479d1f9e3f
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-07-10 20:34:00 +0530

    showing each task in its own expansion panel; might change later

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrl: './task-item.component.scss'
+  styleUrl: './task-item.component.scss',
 })
-export class TaskItemComponent {
+export class TaskItemComponent implements OnInit {
+  @Input() taskName: string = '';
+  shortTaskName = signal('');
 
+  MAX_TASK_LENGTH = 50;
+
+  ngOnInit(): void {
+    let shortenedTask = this.taskName.substring(0, this.MAX_TASK_LENGTH);
+    if (shortenedTask !== this.taskName) {
+      shortenedTask += '...';
+    }
+    this.shortTaskName.set(shortenedTask);
+  }
 }
