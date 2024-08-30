3fc843cf4c301283d604eebefbe7620762368c9e
Md Rafi Akhtar <alimdrafi@gmail.com>
2024-08-30 22:04:42 +0530

    implemented a good chunk of task item like enter on save, click for edit and save, shortening the taks if it's too long, its html and css, and using it in task list

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, OnInit, signal } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  signal,
+  ViewChild,
+} from '@angular/core';
+import { Task } from '../../models/task.model';
+import { TaskManagementService } from '../../services/task-management.service';
 
 @Component({
   selector: 'app-task-item',
@@ -6,16 +15,53 @@ import { Component, Input, OnInit, signal } from '@angular/core';
   styleUrl: './task-item.component.scss',
 })
 export class TaskItemComponent implements OnInit {
-  @Input() taskName: string = '';
+  @Input() task!: Task;
+  @ViewChild('editTaskField') editInput!: ElementRef;
+
   shortTaskName = signal('');
+  isEditingTask = signal(false);
 
   MAX_TASK_LENGTH = 50;
 
+  constructor(private tms: TaskManagementService) {}
+
   ngOnInit(): void {
-    let shortenedTask = this.taskName.substring(0, this.MAX_TASK_LENGTH);
-    if (shortenedTask !== this.taskName) {
+    let shortenedTask = this.task.name.substring(0, this.MAX_TASK_LENGTH);
+    if (shortenedTask !== this.task.name) {
       shortenedTask += '...';
     }
     this.shortTaskName.set(shortenedTask);
+  }
+
+  taskEditorInputKeyPress(evt: KeyboardEvent) {
+    if (evt.ctrlKey && evt.key === 'Enter') {
+      this.taskEditorClick('save');
+    }
+  }
+
+  taskEditorClick(action: 'edit' | 'save' | 'discard') {
+    this.isEditingTask.update((val) => !val);
+
+    switch (action) {
+      case 'edit':
+        setTimeout(() => {
+          this.editInput?.nativeElement.focus();
+        }, 0);
+        break;
+
+      case 'save':
+        const newTask: Task = {
+          ...this.task,
+          name: this.editInput?.nativeElement.value || this.task.name,
+        };
+        this.tms.updateTaskWithIdTo(newTask);
+        break;
+
+      case 'discard':
+        break;
+
+      default:
+        break;
+    }
   }
 }
